34ebc82c84197c8b6c0271c86a60efdd0ab6bb17
zjsxply <54236626+zjsxply@users.noreply.github.com>
2026-04-26 14:08:43 +0000

    Deploying to gh-pages from @ zjsxply/panly.github.io@6cdda63b7bc2af342d8b7e45a10addd0dab2bf55 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "zjsxply/panly.github.io",
+    "data-repo": "",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
